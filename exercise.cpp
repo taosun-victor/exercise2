86c401ab368be7186fabf945b33d81e206d16218
Tao Sun <ts1000262908@c02w66jchtd5.sdcorp.global.sandisk.com>
2019-07-01 09:52:18 -0700

    add next step

--- exercise.cpp
+++ exercise.cpp
@@ -9,6 +9,11 @@ int exercises::say_hello(int input){
     cout << "saying hello " << input_str << endl;
 }
 
+void exercises::tell_next_step(int next) {
+    string next_str = to_string(next);
+    cout << "the next step is " << next_str << endl;
+}
+
 int main() {
     exercises * a = new exercises();
     a->say_hello(5);
--- exercise.h
+++ exercise.h
@@ -3,4 +3,6 @@ class exercises {
         int name;
 	int age;
         int say_hello(int input);
+        void tell_next_step(int next);
+
 };
