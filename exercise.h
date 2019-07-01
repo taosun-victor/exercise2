22d78bcdfc3fb956e75e8b52b07fdad09f79d8ed
Tao Sun <ts1000262908@c02w66jchtd5.sdcorp.global.sandisk.com>
2019-07-01 10:23:38 -0700

    add constructor

--- exercise.h
+++ exercise.h
@@ -5,4 +5,9 @@ class exercises {
         int say_hello(int input);
         void tell_next_step(int next);
 	void add_one_more(void);
+
+        exercises(){
+	    this->name = 34;
+	    this->age = 22;
+	}
 };
